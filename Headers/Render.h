c35d1a5a951adaf47749957ad09b0989c81b4341
Fenix7667 <46452240+Fenix7667@users.noreply.github.com>
2020-07-20 14:01:51 -0300

    Update Render.h

--- Headers/Render.h
+++ Headers/Render.h
@@ -7,17 +7,15 @@
 
 namespace FenixEngine {
 	void drawVertexArray(std::vector<unsigned int> indices) {
-		std::vector<unsigned int>* indicesPointer = &indices;
-
 		glDrawElements(GL_TRIANGLES, (std::size(indices)), GL_UNSIGNED_INT, 0);
 	};
 
 	std::vector<float> getDefaultVertexArray() {
 		std::vector<float> vertexArray = {
-			-0.5, -0.5, // 0
-			 0.5, -0.5, // 1
-			 0.5,  0.5, // 2
-		    -0.5,  0.5, // 3
+			  0,   0.5, // 0
+			  0.5, 0.0, // 1
+			 -0.5, 0.0, // 2
+			  0,  -0.5, // 3
 		};
 
 		return vertexArray;
@@ -26,7 +24,7 @@ namespace FenixEngine {
 	std::vector<unsigned int> getDefaultIndicesArray() {
 		std::vector<unsigned int> indicesArray = {
 			0, 1, 2,
-			2, 3, 0,
+			1, 2, 3,
 		};
 
 		return indicesArray;
