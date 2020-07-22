5c21c9470f29589cf8021cdf0c60ba4d1e0fe7ea
Fenix7667 <46452240+Fenix7667@users.noreply.github.com>
2020-07-21 21:20:36 -0300

    Update Render.h

--- Headers/Render.h
+++ Headers/Render.h
@@ -1,32 +1,30 @@
 #pragma once
-#include <GL/glew.h>;
+
+#include <glad/glad.h>;
 #include <GLFW/glfw3.h>;
+
+#include "../Headers/ComplementaryFunctions.h";
+#include "../Headers/Buffers.h";
 
 #include <vector>;
 #include <string>;
 
 namespace FenixEngine {
-	void drawVertexArray(std::vector<unsigned int> indices) {
-		glDrawElements(GL_TRIANGLES, (std::size(indices)), GL_UNSIGNED_INT, 0);
+	std::vector<unsigned int> indicesArray = {
+		
 	};
 
-	std::vector<float> getDefaultVertexArray() {
-		std::vector<float> vertexArray = {
-			  0,   0.5, // 0
-			  0.5, 0.0, // 1
-			 -0.5, 0.0, // 2
-			  0,  -0.5, // 3
-		};
-
-		return vertexArray;
+	std::vector<float> vertexArray = {
+	  
 	};
 
-	std::vector<unsigned int> getDefaultIndicesArray() {
-		std::vector<unsigned int> indicesArray = {
-			0, 1, 2,
-			1, 2, 3,
-		};
+	void createObject(std::vector<unsigned int> indicesArray, std::vector<float> vertexArray) {
+	  	FenixEngine::indicesArray = indicesArray;
+	    FenixEngine::vertexArray = vertexArray;
 
-		return indicesArray;
+	 //	FenixEngine::indicesArray.insert(FenixEngine::indicesArray.end(), indicesArray.begin(), indicesArray.end());
+	 //	FenixEngine::vertexArray.insert(FenixEngine::vertexArray.end(), vertexArray.begin(), vertexArray.end());
+
+		FenixEngine::createBuffers(indicesArray, vertexArray);
 	};
 };
