8b1bba17b25ffe30352251c07a2cec54b0702100
Fenix7667 <46452240+Fenix7667@users.noreply.github.com>
2020-07-21 21:20:01 -0300

    Update Buffers.h

--- Headers/Buffers.h
+++ Headers/Buffers.h
@@ -1,11 +1,12 @@
 #pragma once
-#include <GL/glew.h>;
+
+#include <glad/glad.h>;
 #include <GLFW/glfw3.h>;
 
 #include <vector>;
- 
+
 namespace FenixEngine {
-	void initializeBuffers(std::vector<float> positions, std::vector<unsigned int> indices) {
+    void createBuffers(std::vector<unsigned int> indicesArray, std::vector<float> vertexArray) {
         GLuint vao = 0;
         glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
@@ -13,7 +14,7 @@ namespace FenixEngine {
         unsigned int buffer;
         glGenBuffers(1, &buffer);
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(float), positions.data(), GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, vertexArray.size() * sizeof(float), vertexArray.data(), GL_STATIC_DRAW);
 
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
@@ -21,6 +22,6 @@ namespace FenixEngine {
         unsigned int indexBufferObject;
         glGenBuffers(1, &indexBufferObject);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(float), indices.data(), GL_STATIC_DRAW);
-	};
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesArray.size() * sizeof(float), indicesArray.data(), GL_STATIC_DRAW);
+    };
 };
